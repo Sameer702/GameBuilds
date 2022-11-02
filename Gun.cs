bffa5d953bdfecbdde6a4188791c8c58a6d75ca3
Mahdi <59067193+MahdiA64@users.noreply.github.com>
2022-11-02 09:07:55 +0000

    Add files via upload

---
+++ Gun.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+
+    public Camera fpscam;
+    public ParticleSystem muzzleFlash;
+
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+       if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.takeDamage(damage);
+            }
+        }
+    }
+}
