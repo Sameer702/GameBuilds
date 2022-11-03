85127d8b232b2f89b5f7a0d005812653a90bedfa
Sameer702 <78430841+Sameer702@users.noreply.github.com>
2022-11-03 14:54:39 +0000

    Health and hunger

---
+++ Assets/_Scripts/PlayerHealthHunger/Decay.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Decay : MonoBehaviour
+{
+    public int decayPercent;
+    public int decayAmount;
+    public float decayTime;
+    public Text decayText;
+    public bool decaying;
+    public Player player;
+
+    void Start()
+    {
+        StartCoroutine(decay());
+    }
+
+    void Update()
+    {
+        decayText.text = decayPercent + "%";
+    }
+    IEnumerator decay()
+    {
+        while (decaying)
+        {
+            yield return new WaitForSeconds(decayTime);
+            
+            if (decayPercent > 0)
+            {
+                decayPercent = decayPercent - decayAmount;
+            }
+            if (decayPercent < 10)
+            {
+                player.takeDamage(10);
+            }
+        }
+    }
+}
